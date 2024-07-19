5080313d39e142cc2235ec06b214448248300f52
CharltonCastlemain <charlton.castlemain1315@gmail.com>
2024-07-18 23:29:00 -0500

    record upload

--- scripts/passwordMatch.js
+++ scripts/passwordMatch.js
@@ -1,20 +1,20 @@
-const pw1 = document.querySelector("#password");
-const pw2 = document.querySelector("#confirm_password");
-const message = document.querySelector("#formmessage");
+document.addEventListener("DOMContentLoaded", function() {
+    const pw1 = document.querySelector("#password");
+    const pw2 = document.querySelector("#confirm_password");
+    const message = document.querySelector("#formmessage");
 
-kp2.addEventListener("focusout", checkSame);
+    pw2.addEventListener("blur", checkPasswordMatch);
 
-// This should be refactored.
-function checkSame() {
-	if (pw1.value !== pw2.value) {
-		message.textContent = "❗Passwords DO NOT MATCH!";
-		message.style.visibility = "show";
-		pw2.style.backgroundColor = "#fff0f3";
-		pw2.value = "";
-		pw2.focus();
-	} else {
-		message.style.display = "none";
-		pw2.style.backgroundColor = "#fff";
-		pw2.style.color = "#000";
-	}
-}
+    function checkPasswordMatch() {
+        if (pw1.value !== pw2.value) {
+            message.textContent = "❗ Passwords DO NOT MATCH!";
+            message.style.visibility = "visible";
+            pw2.style.backgroundColor = "#fff0f3";
+            pw2.value = "";
+            pw2.focus();
+        } else {
+            message.style.visibility = "hidden";
+            pw2.style.backgroundColor = "#fff";
+        }
+    }
+});
